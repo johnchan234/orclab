dc0bb5da5e8df00431e5e98b9886bc7835ac878a
him1019 <hoihim1019@gmail.com>
2022-02-13 12:39:51 +0800

    update

--- genTrainData.py
+++ genTrainData.py
@@ -26,66 +26,6 @@ diamonds (♦) = 1
 hearts (♥) = 2
 spades (♠) = 3
 """
-cardToLabel = {
-    '0_a': "0",
-    '0_2': "1",
-    '0_3': "2",
-    '0_4': "3",
-    '0_5': "4",
-    '0_6': "5",
-    '0_7': "6",
-    '0_8': "7",
-    '0_9': "8",
-    '0_10': "9",
-    '0_j': "10",
-    '0_q': "11",
-    '0_k': "12",
-
-    '1_a': "13",
-    '1_2': "14",
-    '1_3': "15",
-    '1_4': "16",
-    '1_5': "17",
-    '1_6': "18",
-    '1_7': "19",
-    '1_8': "20",
-    '1_9': "21",
-    '1_10': "22",
-    '1_j': "23",
-    '1_q': "24",
-    '1_k': "25",
-
-    '2_a': "26",
-    '2_2': "27",
-    '2_3': "28",
-    '2_4': "29",
-    '2_5': "30",
-    '2_6': "31",
-    '2_7': "32",
-    '2_8': "33",
-    '2_9': "34",
-    '2_10': "35",
-    '2_j': "36",
-    '2_q': "37",
-    '2_k': "38",
-
-    '3_a': "39",
-    '3_2': "40",
-    '3_3': "41",
-    '3_4': "42",
-    '3_5': "43",
-    '3_6': "44",
-    '3_7': "45",
-    '3_8': "46",
-    '3_9': "47",
-    '3_10': "48",
-    '3_j': "49",
-    '3_q': "50",
-    '3_k': "51",
-}
-
-# cardW = 53
-# cardH = 78
 
 imgW = 720
 imgH = 720
@@ -109,7 +49,7 @@ if not os.path.exists("./image/genTrainData/labels/val"):
 
 
 """---------------------------------------------------------------------"""
-genHowMany = 5
+
 
 """ Load BG"""
 bg_pck_fn = "./image/pck/backgrounds.pck"
@@ -130,17 +70,6 @@ sY = 2
 cardW = 200
 cardH = 300
 
-seq = iaa.Sequential([
-    iaa.Affine(scale=[0.65]),
-    iaa.Affine(rotate=(-180, 180)),
-    iaa.Affine(translate_percent={
-        "x": (0, 0.25), "y":  (0, 0.25)}),
-])
-
-
-def findCardName(name):
-    t = name.split("_")
-    return cardToLabel[t[0]+"_"+t[1]]
 
 
 def current_milli_time():
@@ -150,71 +79,6 @@ def current_milli_time():
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-
-def createKeyPointList(list):
-    key = []
-    for p in list:
-        key.append(ia.Keypoint(x=p[0], y=p[1]))
-    return key
-
-
-def createFitSizeEmptyCard(cardFromPck):
-    newSizeCard = np.zeros((imgH, imgW, 4), dtype=np.uint8)
-    newSizeCard[sY:sY+cardH, sX:sX+cardW, :] = cardFromPck
-    return newSizeCard
-
-
-def getKp(left, right, shape):
-    return [
-        [left[0][0],  left[0][1]],
-        [left[1][0],  left[1][1]],
-        [left[2][0],  left[2][1]],
-        [left[3][0],  left[3][1]],
-
-        [right[0][0],  right[0][1]],
-        [right[1][0],  right[1][1]],
-        [right[2][0],  right[2][1]],
-        [right[3][0],  right[3][1]],
-
-        [0, 0],
-        [shape[1], 0],
-        [0, shape[0]],
-        [shape[1], shape[0]],
-
-    ]
-
-
-def createCardAndRandomPlace(seqOption, newSizeCard, kplist):
-    random.seed(datetime.now())
-    seqOptionDefault = [
-        iaa.Affine(scale=[0.5, 0.7])
-        # iaa.Affine(scale=[0.7])
-    ]
-    ##iaa.Affine(rotate=(-10, 10)),
-
-    seq = iaa.Sequential(seqOptionDefault+seqOption)
-
-    seq.to_deterministic()
-    kps = ia.KeypointsOnImage(createKeyPointList(
-        kplist), shape=newSizeCard.shape)
-
-    imageAug, kps_aug = seq(image=newSizeCard, keypoints=kps)
-    bb = Utils.afterRotaKeyPointToBB(kps_aug.keypoints[0:4], imgW, imgH)
-    bbdown = Utils.afterRotaKeyPointToBB(
-        kps_aug.keypoints[4:8], imgW, imgH)
-
-    bbcenter = Utils.afterRotaKeyPointToBB(
-        kps_aug.keypoints[8:12], imgW, imgH)
-
-    mask = imageAug[:, :, 3]  # alpha層 img1 除了有牌的部分其他的全為0，包括alpha層
-    mask = np.stack([mask]*3, -1)  # 堆三個alpha層出來w*h*channel(3)
-
-    return imageAug, bb, bbdown, bbcenter, mask
-
-
-def toYolov5LabelFormat(s1, s2, s3, s4, s5):
-    txt = s1+" "+s2+" "+s3 + " "+s4+" "+s5+"\n"
-    return txt
 
 
 genMax = 100
